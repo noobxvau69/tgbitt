187dbd1b20edcda4f1141b60ccdd968c9a772086
noobxvau69 <mnshiddik1@gmail.com>
2024-10-10 14:59:15 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     dashboardPassword: "your-password",
-    logType: "json",
+    logType: "tgBot",
     note: "Change this to 'mongodb', 'tgBot' or 'json' as needed",
     timezone: "Asia/Dhaka",
     bot: {
